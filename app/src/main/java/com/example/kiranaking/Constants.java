aec993f20b74246d6aecead87f417c981f2c425d
Gagan-nmims <76172276+Gagan-nmims@users.noreply.github.com>
2021-06-26 15:03:13 +0530

    Project based on android named Kirana King for all the local grocery shops and its customers

---
+++ app/src/main/java/com/example/kiranaking/Constants.java
@@ -0,0 +1,36 @@
+package com.example.kiranaking;
+
+public class Constants {
+
+    //product categories
+    public static final String[] productCategories = {
+            "Beverages",
+            "Beauty & Personal Cate",
+            "Baby Kids",
+            "Biscuits Snacks & Chocolates",
+            "Breakfast & Dairy",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetables",
+            "Others"
+    };
+
+    public static final String[] productCategories1 = {
+            "All",
+            "Beverages",
+            "Beauty & Personal Cate",
+            "Baby Kids",
+            "Biscuits Snacks & Chocolates",
+            "Breakfast & Dairy",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetables",
+            "Others"
+    };
+}
